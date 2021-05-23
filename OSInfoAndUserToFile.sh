9b4730b98fc388f146331d95275ac3cf6a4a8721
Kilabarus <63799260+Kilabarus@users.noreply.github.com>
2021-05-23 03:55:34 +0300

    Добавил комментарий с описанием задачи

--- OSInfoAndUserToFile.sh
+++ OSInfoAndUserToFile.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+
+# Сохранить информацию о текущей ОС (uname -a) в файл, а затем добавить в него имя текущего пользователя (whoami).
+
 uname -a > ~/OSInfoAndUser
 echo $(whoami) >> ~/OSInfoAndUser
